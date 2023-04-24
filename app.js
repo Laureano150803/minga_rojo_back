9d0300b6b02d0c93f016c6dfbe4d2234bd2cfc8c
Laureano150803 <109369745+Laureano150803@users.noreply.github.com>
2023-04-24 17:00:22 -0500

    M-02 (#7)

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import 'dotenv/config.js'
 import './config/database.js'
 import createError from 'http-errors'
 import express from 'express'
-
+import cors from 'cors'
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
@@ -22,7 +22,7 @@ const app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
