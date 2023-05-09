71fb1c31b533b353dadc3994cdf5052d6b9d507a
Flavio Brito <flabrito43@gmail.com>
2023-05-08 23:41:14 -0300

    fixed

--- app.js
+++ app.js
@@ -1,43 +1,14 @@
-<<<<<<< HEAD
-=======
-// var createError = require('http-errors');
-// var express = require('express');
->>>>>>> 44351fb7bb2ec1c7a54f60f9860cddb613427a07
 import 'dotenv/config.js'
 import './config/database.js'
-
 import createError from 'http-errors'
 import express from 'express'
-<<<<<<< HEAD
-=======
-
->>>>>>> 44351fb7bb2ec1c7a54f60f9860cddb613427a07
 import path from 'path';
 import cors from 'cors'
 import logger from 'morgan';
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-import cors from 'cors'
-=======
-
-import errorHandler from './middlewares/errorHandler.js'
-import notFound from './middlewares/notFound.js'
-
-
->>>>>>> SPRINT-2
 import indexRouter from './routes/index.js';
 import authsRouter from './routes/auth.js';
->>>>>>> 44351fb7bb2ec1c7a54f60f9860cddb613427a07
 import {__dirname} from './utils.js'
 import notFound from './middlewares/notFound.js'; // se importa el error creado en notfound.js
-import errorHandler from './middlewares/errorHandler.js';
-
-
-
-import indexRouter from './routes/index.js';
-import usersRouter from './routes/auth.js';
-import notFound from './middlewares/notFound.js';
 import errorHandler from './middlewares/errorHandler.js';
 
 const app = express();
@@ -50,61 +21,32 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.set('view engine', 'ejs');
 
-<<<<<<< HEAD
 //middlewares nivel aplicacion
 app.use((req, res, next) => {next()})
 
 //middlewares
 app.use(cors())
-=======
-app.use((req,res,next)=>{
-  console.log('logged'); //1
-//una vez que se ejecuta esto 1
-  next() //2
-})
-//pasa a la siguiente ejecucion //2
-app.use(cors()) // middlewars ESTA EJECUCION
->>>>>>> 44351fb7bb2ec1c7a54f60f9860cddb613427a07
 app.use(logger('dev'));
 app.use(express.json());//verifica la carga util 
 app.use(express.urlencoded({ extended: false }));
-<<<<<<< HEAD
-app.use(express.static(path.join(__dirname, 'public')));
-=======
-app.use(cookieParser());
+/* app.use(cookieParser()); */
 app.use(express.static(path.join(__dirname, 'public'))); //HASTA QUE SE EJECUTE LA TAREA PRINCIPAL QUE ES LA RESPUESTA
 //__dirname es la direccion absolurta donde se encuentra la raiz de la ubicacion, express le hace la peticion se usa para servir los archivos estaticos en el navegador 
 
 
->>>>>>> 44351fb7bb2ec1c7a54f60f9860cddb613427a07
 
 //routers
 app.use('/', indexRouter);
-<<<<<<< HEAD
+
 app.use(notFound)
 app.use(errorHandler)
-=======
-<<<<<<< HEAD
+
+
 app.use('/auth', authsRouter);
 app.use(notFound)//si no hay ruta ejecuta eror 404
 app.use(errorHandler)//
 // se borra lo que trae por defecto 
->>>>>>> 44351fb7bb2ec1c7a54f60f9860cddb613427a07
 
-
-
-<<<<<<< HEAD
 
 export default app;
-=======
-export default app;
-=======
 
-
-
-app.use(notFound) 
-app.use(errorHandler)
-
-export default app;
->>>>>>> SPRINT-2
->>>>>>> 44351fb7bb2ec1c7a54f60f9860cddb613427a07
--- routes/chapters.js
+++ routes/chapters.js
@@ -1,9 +1,6 @@
 //aca voy a definir los endpoints de los autores 
 //y los voy a exportar para poder utilizarlos 
 import { Router } from "express";
-<<<<<<< HEAD
-import read from '../controller/chapters/read.js'
-=======
 import create from "../controllers/chapters/create.js"
 import read from '../controllers/chapters/read.js'
 import validator from "../middlewares/validator.js";
@@ -12,8 +9,6 @@ import chapterExists from "../middlewares/accountChapter.js";
 import orderExists from "../middlewares/exists_order.js";
 import nextOrder from "../middlewares/next_order.js";
 
-let router =Router()
->>>>>>> SPRINT-2
 
 let router = Router()
 
@@ -22,10 +17,6 @@ router.get('/', read)
 // router.put('/:id', (req, res, next) => res.status(200).send('autores modificados'))
 // router.delete('/:id', (req, res, next) => res.status(200).send('autores borrados'))
 
-<<<<<<< HEAD
-export default router;
-=======
 router.post('/',validator(chapterCreate), chapterExists,orderExists,nextOrder,  create)
 
 export default router
->>>>>>> SPRINT-2
