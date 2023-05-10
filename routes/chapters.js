2142040da5bc602c79c50078116fcb25c50334ec
unknown <laureanohurtado1@gmail.com>
2023-05-10 09:11:02 -0500

    m-9

--- routes/chapters.js
+++ routes/chapters.js
@@ -2,17 +2,19 @@
 //y los voy a exportar para poder utilizarlos 
 import { Router } from "express";
 import create from "../controllers/chapters/create.js"
-import read from "../controllers/chapters/get_one.js";
+import get_one from "../controllers/chapters/get_one.js";
 import validator from "../middlewares/validator.js";
 import {chapterCreate} from '../schemas/chapters.js'
 import chapterExists from "../middlewares/accountChapter.js";
 import orderExists from "../middlewares/exists_order.js";
 import nextOrder from "../middlewares/next_order.js";
+import read from "../controllers/chapters/read.js";
 
 
 let router = Router()
 
 // router.post('/', (req, res, next) => res.status(200).send('author creado'))
+router.get('/:id', get_one)
 router.get('/', read)
 // router.put('/:id', (req, res, next) => res.status(200).send('autores modificados'))
 // router.delete('/:id', (req, res, next) => res.status(200).send('autores borrados'))
