ae922ca0e64c65afad7e7156350db30d71594e72
jdosing <96783149+jdosing@users.noreply.github.com>
2023-05-10 09:05:17 -0500

    tarea m-08 (#13)

--- controllers/mangas/read.js
+++ controllers/mangas/read.js
@@ -1,17 +1,14 @@
-import Manga from "../../models/Manga.js";
+import Manga from "../../models/Manga.js"
 
-let read = async (req, res, next) => {
-    try {
-        let all = await Manga.find({
-            title:{"$regex":'ga',}
-        },'-_id')
-        return res.status(200)
-        .json({
-            success: true,
-            response: all
-        })
-    }catch (error) {
-        next(error)
-    }
-}
-export default read
+let read = async(req, res, next) => {
+  try{
+    let all = await Manga.find()
+    return res.status(200).json({
+      Manga: all
+    })
+  }catch(error){
+    next(error)
+  }
+} 
+
+  export default read;
--- routes/chapters.js
+++ routes/chapters.js
@@ -2,20 +2,17 @@
 //y los voy a exportar para poder utilizarlos 
 import { Router } from "express";
 import create from "../controllers/chapters/create.js"
-import read from "../controllers/chapters/get_one.js";
+// import read from "../controllers/chapters/get_one.js";
 import validator from "../middlewares/validator.js";
 import {chapterCreate} from '../schemas/chapters.js'
 import chapterExists from "../middlewares/accountChapter.js";
 import orderExists from "../middlewares/exists_order.js";
 import nextOrder from "../middlewares/next_order.js";
+import get_chapters from "../controllers/chapters/get_chapters.js";
 
 
 let router = Router()
-
-// router.post('/', (req, res, next) => res.status(200).send('author creado'))
-router.get('/', read)
-// router.put('/:id', (req, res, next) => res.status(200).send('autores modificados'))
-// router.delete('/:id', (req, res, next) => res.status(200).send('autores borrados'))
+// router.get('/', read)
 router.post('/',validator(chapterCreate), chapterExists,orderExists,nextOrder,  create)
-
+router.get('/',get_chapters)
 export default router
