d45d0bb5f7209f34558064a5e49d9ca4469f2173
Flavio Brito <flabrito43@gmail.com>
2023-05-15 20:57:35 -0300

    fixed

--- routes/chapters.js
+++ routes/chapters.js
@@ -2,21 +2,24 @@
 //y los voy a exportar para poder utilizarlos 
 import { Router } from "express";
 import create from "../controllers/chapters/create.js"
-import read from "../controllers/chapters/get_one.js";
+import get_one from "../controllers/chapters/get_one.js";
 import validator from "../middlewares/validator.js";
 import {chapterCreate} from '../schemas/chapters.js'
 import chapterExists from "../middlewares/accountChapter.js";
 import orderExists from "../middlewares/exists_order.js";
 import nextOrder from "../middlewares/next_order.js";
-import get_one from "../controllers/chapters/get_one.js";
-import get_chapters from "../controllers/chapters/get_chapters.js";
+import read from "../controllers/chapters/read.js";
+import get_chapters from '../controllers/chapters/get_chapters.js'
+
+
 
 
 let router = Router()
 
-router.get('/:id', get_one)
 // router.post('/', (req, res, next) => res.status(200).send('author creado'))
 router.get('/', read)
+router.get('/:id', get_one)
+
 // router.put('/:id', (req, res, next) => res.status(200).send('autores modificados'))
 // router.delete('/:id', (req, res, next) => res.status(200).send('autores borrados'))
 router.post('/',validator(chapterCreate), chapterExists,orderExists,nextOrder,  create)
